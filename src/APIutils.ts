f8c37a1dd5961bfd86cab7a6ca134691f0e7f05f
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-11-14 01:15:50 +0000

    remove timeouts used for testing

--- src/APIutils.ts
+++ src/APIutils.ts
@@ -1,21 +1,13 @@
 async function fetchAllCountries() {
   const response = await fetch('https://restcountries.com/v3.1/all?fields=name,capital,population,flags,region,cca3');
   const allCountries = await response.json();
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(allCountries);
-    }, 2000); // Simulate a slow network request
-  });
+  return allCountries;
 }
 
 async function fetchCountry(countryCode: string) {
   const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}?fields=name,capital,population,flags,region,subregion,cca3,borders,languages,currencies,tld,independent,unMember`);
   const country = await response.json();
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(country);
-    }, 2000); // Simulate a slow network request
-  });
+  return country;
 }
 
 export { fetchAllCountries, fetchCountry };
